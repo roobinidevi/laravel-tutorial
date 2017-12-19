0da5208d3a4d04c8115573e60223a42d0abbeb59
roobinidevi <31208856+roobinidevi@users.noreply.github.com>
2017-12-19 15:21:41 +0530

    Change password

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,26 +3,56 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Http\Response;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
+use function bcrypt;
+use function redirect;
+use function view;
 
-class HomeController extends Controller
-{
+class HomeController extends Controller {
+
     /**
      * Create a new controller instance.
      *
      * @return void
      */
-    public function __construct()
-    {
+    public function __construct() {
         $this->middleware('auth');
     }
 
     /**
      * Show the application dashboard.
      *
-     * @return \Illuminate\Http\Response
+     * @return Response
      */
-    public function index()
-    {
+    public function index() {
         return view('home');
     }
+
+    public function showChangePasswordForm() {
+        return view('auth.changepassword');
+    }
+
+    public function changepassword(Request $request) {
+
+        $request->validate([
+            'current-password' => 'required',
+            'new-password' => 'required|different:current-password',
+            'confirm-password' => 'required',
+        ]);
+
+        if (!(Hash::check($request->get('current-password'), Auth::user()->password))) {
+            // The passwords matches
+            return redirect()->back()->with("error", "Your current password does not matches with the password you provided. Please try again.");
+        }
+        if($request->get('new-password') != $request->get('confirm-password')) {
+            return redirect()->back()->with("error","New Password and Confirm Password must be same");
+        }
+        $user = Auth::user();
+        $user->password = bcrypt($request->get('new-password'));
+        $user->save();
+        return redirect()->back()->with("success", "Password changed successfully !");
+    }
+
 }
